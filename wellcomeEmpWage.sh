cd2dc6d55f8f726d346a91ccb57f8fd35c1bb44e
ruchita-patil <ruchita.patil@catseyesystems.com>
2020-09-24 11:05:18 +0530

    Employee Attendance program

--- wellcomeEmpWage.sh
+++ wellcomeEmpWage.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "WELCOME TO EMPLOYEE WAGE COMPUTION PROGRAM";
+echo "WELCOME TO EMPLOYEE WAGE COMPUTION PROGRAM"; 
+
+
+value=$((RANDOM%2)); 
+echo $value; 
+
+if [ $value == 1 ] 
+then
+	echo "Employee is Present"; 
+else 
+	echo "Employee is Absent";
+fi
