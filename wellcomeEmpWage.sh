2ca24232ce6c9396e336099cf22e867357f09e7d
ruchita-patil <ruchita.patil@catseyesystems.com>
2020-09-24 11:30:28 +0530

    Employee Daily Wage.

--- wellcomeEmpWage.sh
+++ wellcomeEmpWage.sh
@@ -8,7 +8,22 @@ echo $value;
 
 if [ $value == 1 ] 
 then
-	echo "Employee is Present"; 
+	echo "Employee is Present";
 else 
-	echo "Employee is Absent";
-fi
+	echo "Employee is Absent"; 
+
+fi;
+
+EMPLOYEE_PER_HOUR_WAGE=20; 
+FULL_DAY_HOUR=8;
+
+#calculation for daily wage
+if [ $value == 1 ] 
+then
+	dailyWage=$((EMPLOYEE_PER_HOUR_WAGE * FULL_DAY_HOUR)); 
+	echo "Daily Wage For an Employee is :"$dailyWage;
+
+else 
+
+	echo "Empwage cannot be calculated for absent employee";
+fi;
